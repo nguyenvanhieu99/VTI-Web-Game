538bc10dc6922f8239d0e7dcd1469b400257cc43
VanHieu <hieuhamhocpro@gmail.com>
2021-08-23 15:23:12 +0700

    add back end project

---
+++ server/src/test/java/VTI/Web_game/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package VTI.Web_game.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
